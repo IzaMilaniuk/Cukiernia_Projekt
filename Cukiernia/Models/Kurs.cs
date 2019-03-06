a9ab75add509ddaac81f7254805d011f95e56ea0
iza <izamilaniuk@gmail.com>
2019-03-06 17:55:38 +0100

    dodanie classy

---
+++ Cukiernia/Models/Kurs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cukiernia.Models
+{
+    public class Kurs
+    {
+        public int KursId { get; set }
+    }
+}
