725315b0be78132c3fd6c50f51838d48785aae41
iza <izamilaniuk@gmail.com>
2019-07-01 20:23:10 +0200

    Odcinek 15 - Dodawanie produktów do koszyka , ilości w koszyku.

--- Cukiernia/Controllers/KoszykController.cs
+++ Cukiernia/Controllers/KoszykController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Cukiernia.DAL;
+using Cukiernia.Infrastructure;
+using Cukiernia.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +11,37 @@ namespace Cukiernia.Controllers
 {
     public class KoszykController : Controller
     {
+        private KoszykMenager koszykMenager;
+        private ISessionMenager sessionMenager { get; set; }
+        private ProduktyContext db ;
+
+        public KoszykController()
+        {
+            db = new ProduktyContext();
+            sessionMenager = new SessionMenager();
+            koszykMenager = new KoszykMenager(sessionMenager,db);
+        }
         // GET: Koszyk
         public ActionResult Index()
         {
-            return View();
+            var pozycjeKoszyka = koszykMenager.PobierzKoszyk();
+            var cenaCalowita = koszykMenager.PobierzWartoscKoszyka();
+            KoszykViewModel koszykVM = new KoszykViewModel()
+            {
+                PozycjeKoszyka =pozycjeKoszyka,
+                CenaCalkowita =cenaCalowita
+            };
+            return View(koszykVM);
         }
-        public ActionResult DodajDoKoszyka(string id)
+        public ActionResult DodajDoKoszyka(int id)
         {
-            return View();
+            koszykMenager.DodajDoKoszyka(id);
+            return RedirectToAction("Index");
+        }
+        public int PobierzIloscElementowKoszyka()
+        {
+           return koszykMenager.PobierzIloscPozycjiKoszyka();
+
         }
     }
 }
---
+++ Cukiernia/ViewModels/KoszykViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Cukiernia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cukiernia.ViewModels
+{
+    public class KoszykViewModel
+    {
+        public List<PozycjaKoszyka> PozycjeKoszyka { get; set; }
+        public decimal CenaCalkowita { get; set; }
+
+    }
+}
