e6aaec3cb8b892400d31fac8fdd8cab8f24f70bd
iza <izamilaniuk@gmail.com>
2019-07-09 19:15:53 +0200

    Koniec odc 16- Dodanie widoków rejestracji i logowania- dziala

--- Cukiernia/Controllers/AccountController.cs
+++ Cukiernia/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Cukiernia.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,9 +11,36 @@ namespace Cukiernia.Controllers
     public class AccountController : Controller
     {
         // GET: Account
-        public ActionResult Login()
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            else
+
+                return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            else
+                return RedirectToAction("Index", "Home");
+        }
+
+
+
     }
 }
