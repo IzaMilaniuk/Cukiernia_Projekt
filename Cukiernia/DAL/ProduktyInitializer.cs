4fa620ca613c95d4bd8fdaa3924a3c8efd45d6d3
iza <izamilaniuk@gmail.com>
2019-10-08 23:00:27 +0200

    Wszystko działa- wysyłanie emaili itp , ikonki nowe

--- Cukiernia/DAL/ProduktyInitializer.cs
+++ Cukiernia/DAL/ProduktyInitializer.cs
@@ -12,40 +12,41 @@ using System.Web;
 namespace Cukiernia.DAL
 {
     //migrowanie bazy do najnowszej wersji
-    public class ProduktyInitializer : MigrateDatabaseToLatestVersion <ProduktyContext, Configuration>
+    public class ProduktyInitializer : MigrateDatabaseToLatestVersion<ProduktyContext, Configuration>
     {
-      
+
 
         //Dodanie do bazy przykladowe dane
         public static void SeedKProduktyData(ProduktyContext context)
         {
             var kategorie = new List<Kategoria>
             {
-                new Kategoria() { KategoriaId=1, NazwaKategorii="Pieczywo", NazwaPlikuIkony="aspnet.png", OpisKategorii="programowanie w asp net" },
-                new Kategoria() { KategoriaId=2, NazwaKategorii="Ciasta", NazwaPlikuIkony="javascript.png", OpisKategorii="skryptowy język programowania" },
-                new Kategoria() { KategoriaId=3, NazwaKategorii="Fit Ciasta", NazwaPlikuIkony="jquery.png", OpisKategorii="lekka biblioteka programistyczna dla języka JavaScript" },
-                new Kategoria() { KategoriaId=4, NazwaKategorii="Ciasteczka", NazwaPlikuIkony="html.png", OpisKategorii="język wykorzystywany do tworzenia i prezentowania stron internetowych www" },
-                new Kategoria() { KategoriaId=5, NazwaKategorii="Nowości", NazwaPlikuIkony="css.png", OpisKategorii="język służący do opisu formy prezentacji (wyświetlania) stron www" },
-                new Kategoria() { KategoriaId=6, NazwaKategorii="Babeczni", NazwaPlikuIkony="xml.png", OpisKategorii="uniwersalny język znaczników przeznaczony do reprezentowania różnych danych w strukturalizowany sposób" },
-       
+                new Kategoria() { KategoriaId=1, NazwaKategorii="Pieczywo", NazwaPlikuIkony="pieczywo.png", OpisKategorii="Pieczywo" },
+                new Kategoria() { KategoriaId=2, NazwaKategorii="Ciasta", NazwaPlikuIkony="ciasto.png", OpisKategorii="Ciasta" },
+                new Kategoria() { KategoriaId=3, NazwaKategorii="Fit Ciasta", NazwaPlikuIkony="ciasto-fit.png", OpisKategorii="Ciasto Dietetyczne" },
+                new Kategoria() { KategoriaId=4, NazwaKategorii="Ciasteczka", NazwaPlikuIkony="ciastko.png", OpisKategorii="Ciasta" },
+                new Kategoria() { KategoriaId=5, NazwaKategorii="Nowości", NazwaPlikuIkony="nowosci.png", OpisKategorii="Nowości aktualne" },
+                new Kategoria() { KategoriaId=6, NazwaKategorii="Muffiny", NazwaPlikuIkony="mufinka.png", OpisKategorii="Babeczki" },
+
              };
             kategorie.ForEach(k => context.Kategorie.AddOrUpdate(k));  //każda kategoria z listy jest dodawana do bazy
             context.SaveChanges();
 
             var produkty = new List<Produkt>
             {
-                new Produkt() { ProduktId=1, AutorProdukt="Iza", TytulProdukt="Sernik", KategoriaId=1, CenaProdukt= 40, Bestseller=true, NazwaPlikuObrazka="sernik.png",
+                new Produkt() { ProduktId=1, AutorProdukt="Iza", TytulProdukt="Sernik", KategoriaId=2, CenaProdukt= 40, Bestseller=true, NazwaPlikuObrazka="sernik.png",
                 DataDodania = DateTime.Now, OpisProdukt="super ciacho"},
-                new Produkt() {ProduktId=5, AutorProdukt="Monika", TytulProdukt="Bułka pszenna", KategoriaId=2, CenaProdukt=10, Bestseller=true, NazwaPlikuObrazka="pszenna.png",
+                new Produkt() {ProduktId=5, AutorProdukt="Monika", TytulProdukt="Bułka pszenna", KategoriaId=1, CenaProdukt=10, Bestseller=true, NazwaPlikuObrazka="pszenna.png",
                 DataDodania=DateTime.Now, OpisProdukt="bulka zwykla"},
-                new Produkt() {ProduktId=2, AutorProdukt="Monika", TytulProdukt="Bułka żytnia", KategoriaId=2, CenaProdukt=10, Bestseller=true, NazwaPlikuObrazka="zytnia.png",
+                new Produkt() {ProduktId=2, AutorProdukt="Monika", TytulProdukt="Bułka żytnia", KategoriaId=1, CenaProdukt=10, Bestseller=true, NazwaPlikuObrazka="zytnia.png",
                 DataDodania=DateTime.Now, OpisProdukt="bulka zytnia"},
-                new Produkt() {ProduktId=2, AutorProdukt="Monika", TytulProdukt="Przekładaniec", KategoriaId=1, CenaProdukt=50, Bestseller=true, NazwaPlikuObrazka="przekladaniec.png",
+                new Produkt() {ProduktId=2, AutorProdukt="Monika", TytulProdukt="Przekładaniec", KategoriaId=2, CenaProdukt=50, Bestseller=true, NazwaPlikuObrazka="przekladaniec.png",
                 DataDodania=DateTime.Now, OpisProdukt="Przekładaniec zwykły"},
             };
             produkty.ForEach(k => context.Produkty.AddOrUpdate(k));
             context.SaveChanges();
-            
+
+
         }
         public static void SeedUzytkownicy(ProduktyContext db)
         {
